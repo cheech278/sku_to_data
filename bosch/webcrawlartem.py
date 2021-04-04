82540f908c3251afa79efa1348b7c2c147578ce4
Cheech278 <andrii.k278@gmail.com>
2021-04-04 23:04:42 +0100

    Functions rearrangement

--- bosch/webcrawlartem.py
+++ bosch/webcrawlartem.py
@@ -1,4 +1,4 @@
-import daneTechniczneBosch
+import BoLib
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
@@ -7,21 +7,21 @@ import openpyxl
 import re
 
 wb = openpyxl.load_workbook(filename='test_1.xlsx')
-sheet1=wb['sheet']
-sheet2=wb['Sheet1']
-start=input("Start from ...")
-end=input("End on ...")
-for x in range(abs(int(start)-int(end))):
-    sku=(sheet1["A%s" % str(x+int(start))].value)
+sheet1 = wb['sheet']
+sheet2 = wb['Sheet1']
+start = input("Start from ...")
+end = input("End on ...")
+for x in range(abs(int(start) - int(end))):
+    sku = (sheet1["A%s" % str(x + int(start))].value)
     print(sku)
-    if daneTechniczneBosch.count_table(sku) == 0:
+    if BoLib.count_table(sku) == 0:
         print (str(sku) + "not found")
-    elif daneTechniczneBosch.count_table(sku) > 0:
-        descr= str(daneTechniczneBosch.descrGet_instr(sku)[0])
-        pic = daneTechniczneBosch.PictureGet_instr(sku)
-        daneTech=daneTechniczneBosch.daneTechReturnCat(sku)
-        val=''
-        name = str(daneTechniczneBosch.nameGet_instr(sku))
+    elif BoLib.count_table(sku) > 0:
+        descr = str(BoLib.descrGet_instr(sku)[0])
+        pic = BoLib.PictureGet_instr(sku)
+        daneTech = BoLib.daneTechReturnCat(sku)
+        val = ''
+        name = str(BoLib.nameGet_instr(sku))
         for c in range(1,len(daneTech)-1,2):
             val+=str(daneTech[c]) + ' : ' + str(daneTech[c+1]) + '\n'
         sheet1["B%s" % str(x+int(start))].value = val
@@ -31,5 +31,5 @@ for x in range(abs(int(start)-int(end))):
     
 wb.save("test_1.xlsx")
 
-
+#sleep, try, output, merge with lib
         
